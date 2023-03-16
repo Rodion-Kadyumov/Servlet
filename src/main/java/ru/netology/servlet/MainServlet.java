01949cacac54d4fb198df113521fb22a718fc6f0
Rodion Kadyumov <krrkes@gmail.com>
2023-03-16 09:19:45 +0500

    Переделал в классы

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -10,6 +10,11 @@ import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
     private PostController controller;
+    private static final String GET = "GET";
+    private static final String POST = "POST";
+    private static final String DELETE = "DELETE";
+    private static final String API = "/api/posts";
+    private static final String Match = "/api/posts/\\d+";
 
     @Override
     public void init() {
@@ -25,23 +30,24 @@ public class MainServlet extends HttpServlet {
             final var path = req.getRequestURI();
             final var method = req.getMethod();
             // primitive routing
-            if (method.equals("GET") && path.matches("/api/posts/?$")) {
+            if (method.equals(GET) && path.equals(API)) {
                 controller.all(resp);
                 return;
             }
-            if (method.equals("POST") && path.equals("/api/posts")) {
+            if (method.equals(GET) && path.matches(Match)) {
+                // easy way
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals(POST) && path.equals(API)) {
                 controller.save(req.getReader(), resp);
                 return;
             }
-            if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
+            if (method.equals(DELETE) && path.matches(Match)) {
                 // easy way
-                controller.removeById(getPostID(path), resp);
-                return;
-            }
-
-            if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
-                // easy way
-                controller.getById(getPostID(path), resp);
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+                controller.removeById(id, resp);
                 return;
             }
             resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
@@ -49,9 +55,5 @@ public class MainServlet extends HttpServlet {
             e.printStackTrace();
             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
-    }
-
-    private long getPostID (String path) {
-        return Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
     }
 }
